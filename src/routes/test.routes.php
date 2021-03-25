ad23ab87f2d2cb9b613b588a514025e863cafeb5
Roman Kozin <romanko95.rk@gmail.com>
2021-03-25 23:33:19 +0200

    - Updated test routes for rpc gateway

--- src/routes/test.routes.php
+++ src/routes/test.routes.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('test-method', static function () {
-    return response()
-        ->json([
-            'rpc' => false,
-            'is_adapted' => false
-        ]);
-})->name('rpc.gateway::test_method');
+Route::prefix('rpc-gateway')
+    ->group(static function () {
+        Route::get(
+            'test-method',
+            static function () {
+                return response()
+                    ->json([
+                        'rpc' => false,
+                        'is_adapted' => false
+                    ]);
+            })
+            ->name('rpc.gateway::test_method');
+    });
