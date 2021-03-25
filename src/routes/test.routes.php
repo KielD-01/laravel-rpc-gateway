d33f53a3776107f26f896c3dcf4d58d0edca4ac5
Roman Kozin <romanko95.rk@gmail.com>
2021-03-25 22:39:00 +0200

    - Added test route

---
+++ src/routes/test.routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('test-method', static function () {
+    return response()
+        ->json([
+            'rpc' => false,
+            'is_adapted' => false
+        ]);
+})->name('rpc.gateway::test_method');
